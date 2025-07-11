58ecfcb37da5d43bca33abb5df51809bbacf7839
Dennis Kim <dennis.kim@hotmail.com>
2025-07-11 15:44:50 -0400

    fix infinite loop by using native fetch

--- src/utilities/api.ts
+++ src/utilities/api.ts
@@ -1,24 +1,18 @@
 const BASE_URL = import.meta.env.VITE_API_URL;
 
-export const apiFetch = async (path: string, options = {}) => {
-    console.trace('apiFetch called:', path);
-    const res = await fetch(`${BASE_URL}${path}`, { ...options });
+export const apiFetch = async (
+    path: string,
+    options: RequestInit = {}
+) => {
+    const res = await fetch(`${BASE_URL}${path}`, {
+        credentials: 'include',
+        ...options
+    });
+
+    if (!res.ok) {
+        const error = await res.text();
+        throw new Error(error || 'API error');
+    }
+
     return res.json();
 };
-
-// export const apiFetch = async (
-//     path: string,
-//     options: RequestInit = {}
-// ) => {
-//     const res = await apiFetch(`${BASE_URL}${path}`, {
-//         credentials: 'include',
-//         ...options
-//     });
-
-//     if (!res.ok) {
-//         const error = await res.text();
-//         throw new Error(error || 'API error');
-//     }
-
-//     return res.json();
-// };
